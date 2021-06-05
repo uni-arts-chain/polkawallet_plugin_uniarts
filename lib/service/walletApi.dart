ca0b0ba26c1617056ee0a0339ee0aa2f2a0aab6d
朱彬 <zhubin10811010025@qq.com>
2021-06-05 14:42:19 +0800

    upgrade walletsdk & polkawallet_ui

--- lib/service/walletApi.dart
+++ lib/service/walletApi.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class WalletApi {
   static const String _endpoint = 'https://api.polkawallet.io';
 
   static Future<Map> getRecommended() async {
     try {
-      Response res = await get('$_endpoint/recommended.json');
+      var uri = Uri.parse("$_endpoint/recommended.json");
+      var res = await http.get(uri);
       if (res == null) {
         return null;
       } else {
